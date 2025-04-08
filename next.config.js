7b326a3b59281e28ca4f45a232b49790732f2b82
shivangipatel2508 <shivangipatel2508@gmail.com>
2025-04-08 18:12:14 +0530

    changed the fonts to fix the issue

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
     domains: ['images.unsplash.com'],
   },
   reactStrictMode: true,
-  swcMinify: true,
+  // Removing swcMinify as it's no longer needed in Next.js 15+
+  // It's enabled by default now
 };
 
 module.exports = nextConfig;
