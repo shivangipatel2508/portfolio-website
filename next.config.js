6158aa622c1a3081f36e949c80d480c6896126a7
shivangipatel2508 <shivangipatel2508@gmail.com>
2026-04-26 18:36:13 +0530

    refactor: update image configuration to use remotePatterns and enhance dark mode support in CSS

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   /* config options here */
   images: {
-    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
   reactStrictMode: true,
   // Removing swcMinify as it's no longer needed in Next.js 15+
